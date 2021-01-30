d2afbd9f051ab68b5a3715f7424399c32df9b51e
Voiture-0 <nickcb7@gmail.com>
2021-01-30 13:52:50 -0500

    Add project files.

---
+++ SpeakingTime/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SpeakingTime.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

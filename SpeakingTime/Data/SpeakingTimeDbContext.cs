5de1e3a33ea61779281dd56b3399e14d7a522e85
Voiture-0 <nickcb7@gmail.com>
2021-01-30 15:56:07 -0500

    Added an in memory db context

---
+++ SpeakingTime/Data/SpeakingTimeDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SpeakingTime.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpeakingTime.Data
+{
+    public class SpeakingTimeDbContext : DbContext
+    {
+        public SpeakingTimeDbContext(DbContextOptions<SpeakingTimeDbContext> options)
+          : base(options) { }
+
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Message> Messages { get; set; }
+    }
+}
--- SpeakingTime/Program.cs
+++ SpeakingTime/Program.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using SpeakingTime.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +15,17 @@ namespace SpeakingTime
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            
+            //// Seed database
+            //using (var scope = host.Services.CreateScope())
+            //{
+            //    var services = scope.ServiceProvider;
+            //    var context = services.GetRequiredService<SpeakingTimeDbContext>();
+            //    DataGenerator.Initialize(services);
+            //}
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>

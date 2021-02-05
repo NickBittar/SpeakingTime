7641e795aab12715fa0bd8d06d0323f6e92a3669
Voiture-0 <nickcb7@gmail.com>
2021-02-05 07:01:25 -0500

    Added method to convert DateTimes to client's local timezone

---
+++ SpeakingTime/TagHelpers/LocalDateTimeStringTagHelper.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpeakingTime.TagHelpers
+{
+    public class LocalDateTimeStringTagHelper : TagHelper
+    {
+        public DateTime? DateTime { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            string htmlContent = "";
+            if (DateTime.HasValue)
+            {
+                output.TagName = "script";
+                htmlContent = "(function(){let date=new Date('" + DateTime.Value.ToString("u") + "');document.write(date.toLocaleString());})();";
+            } 
+            else
+            {
+                output.TagName = null;
+            }
+            output.Content.SetHtmlContent(htmlContent);
+        }
+    }
+}
